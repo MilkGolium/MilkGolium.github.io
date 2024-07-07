28c7c8aa0fdf1d964ebf0cbbdde29acf88980d45
MilkGolium <milkgolium@gmail.com>
2024-07-07 09:52:05 +0800

    a commit

--- git.sh
+++ git.sh
@@ -1,11 +1,14 @@
 # git init
-#git add .
-#git commit -m "an commit"
-#git branch -M main
-#git remote add origin https://github.com/MilkGolium/EOP-web.git
-#git push -u origin main
+# git add .
+# git commit -m "an commit"
+# git branch -M main
+# git remote add origin https://github.com/MilkGolium/EOP-web.git
+# git push -u origin main
 
 git add .
+# 如果你第一次运行
+# git config --global user.email xxx@gmail.com
+# git config --global user.name xxx
 git commit -m "a commit"
 git branch -M main
 git push -u origin main
