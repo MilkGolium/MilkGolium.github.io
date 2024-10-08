82727540ff1e50daf49cc7b4d5850f4fc175c4dd
Ensterin Albert <milkgolium@gmail.com>
2024-10-08 21:17:19 +0800

    comi

---
+++ git.sh
@@ -0,0 +1,14 @@
+# git init
+# git add .
+# git commit -m "an commit"
+# git branch -M main
+# git remote add origin https://github.com/MilkGolium/EOP-web.git
+# git push -u origin main
+
+git add .
+# 如果你第一次运行
+# git config --global user.email xxx@gmail.com
+# git config --global user.name xxx
+git commit -m "a commit"
+git branch -M main
+git push -u origin main
